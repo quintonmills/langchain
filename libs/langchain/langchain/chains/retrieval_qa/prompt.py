d1ccbf2917aa40ccbfc85b6ed23d6364ccee23b1
Quinton Mills <41127568+quintonmills@users.noreply.github.com>
2023-11-26 18:17:55 -0500

    Update prompt.py

--- libs/langchain/langchain/chains/retrieval_qa/prompt.py
+++ libs/langchain/langchain/chains/retrieval_qa/prompt.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 from langchain_core.prompts import PromptTemplate
 
-prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say hi
 
 {context}
 
